8ce3d2e30820dec47b7afabc8001756ead77821e
Lucas Emmanuel Mendoza <mendozalucas001@gmail.com>
2025-06-19 14:53:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './features/counter/counterSlice.js'
+import './App.css'
+
+function App() { 
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Contador: {count}</h2>
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>-1</button>
+    </div>
+  )
+}
+
+export default App
